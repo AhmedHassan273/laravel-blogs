b44f0be00bf26f8be17c8310d0d0c5531b659439
unknown <AhmedHassan@ahmed273g@gmail.com>
2018-07-19 00:30:55 +0200

    add link to logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
+    <title>My Blog | Share With the World</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/style.css">
 </head>
