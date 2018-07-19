2f04496a35346178cb5b980dcfa9f780260cbd0d
unknown <AhmedHassan@ahmed273g@gmail.com>
2018-07-19 12:29:08 +0200

    laravel mix

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Blog | Share With the World</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <header>
